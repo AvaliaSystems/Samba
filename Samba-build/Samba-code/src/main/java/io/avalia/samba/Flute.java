9429c31da1eced136b1e56bcb22d44cdbcade619
Eduardo <ebanton@gmail.com>
2017-07-24 11:17:06 -0300

    Fix bug and new feature

---
+++ Samba-build/Samba-code/src/main/java/io/avalia/samba/Flute.java
@@ -0,0 +1,20 @@
+package io.avalia.samba;
+
+public class Flute implements IInstrument {
+
+	@Override
+	public String play() {
+		return null;
+	}
+
+	@Override
+	public int getSoundVolume() {
+		return 3;
+	}
+
+	@Override
+	public String getColor() {
+		return null;
+	}
+
+}
