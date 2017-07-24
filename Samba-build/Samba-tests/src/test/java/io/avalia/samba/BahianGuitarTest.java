0b924df3bb055cbcc68acda983b6ac225e82c319
lfrefatti <lfrefatti@gmail.com>
2017-07-24 11:41:28 -0300

    unit tests for the bahian guittar implementation

---
+++ Samba-build/Samba-tests/src/test/java/io/avalia/samba/BahianGuitarTest.java
@@ -0,0 +1,49 @@
+package io.avalia.samba;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+
+public class BahianGuitarTest {
+
+	/*
+	
+	@Test
+	public void thereShouldBeAnIInstrumentInterfaceAndABahianGuitarClass() {
+		IInstrument bahianGuitar = new BahianGuitar();
+		assertNotNull(bahianGuitar);
+	}
+	
+	@Test
+	public void itShouldBePossibleToPlayABahianGuitar() {
+		IInstrument bahianGuitar = new BahianGuitar();
+		String sound = bahianGuitar.play();
+		assertNotNull(sound);
+	}
+	
+	@Test 
+	public void aBahianGuitarShouldMakeOhMeuRei(){
+		IInstrument bahianGuitar = new BahianGuitar();
+		String sound = bahianGuitar.play();
+		assertEquals("oh meu rei", sound);
+	}
+	
+	@Test 
+	public void aBahianGuitarMustBeAStringInstrument(){
+		IInstrument bahianGuitar = new BahianGuitar();
+		String instrumentClass = bahianGuitar.getInstrumentClass();
+		assertEquals("string", instrumentClass);
+	}
+	
+	@Test 
+	public void aBahianGuitarMustHaveFourOrFiveStrings(){
+		IInstrument bahianGuitar = new BahianGuitar();
+		int stringNumber = bahianGuitar.getStringsNumber();
+		assertTrue(stringNumber == 4 || stringNumber == 5);
+	}
+	
+	*/
+}
