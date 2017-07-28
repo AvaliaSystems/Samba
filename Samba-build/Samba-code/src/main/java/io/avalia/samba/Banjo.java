660c9b0092a1513a3377b153a5de38260687005f
Francisco Kindermann <francisco.kindermann@softplan.com.br>
2017-07-28 10:55:15 -0300

    'Comeco'

---
+++ Samba-build/Samba-code/src/main/java/io/avalia/samba/Banjo.java
@@ -0,0 +1,17 @@
+package io.avalia.samba;
+
+public class Banjo implements IInstrument {
+	
+	public String play() {
+		return null;
+	}
+	
+	public int getSoundVolume() {
+		return 0;
+	}
+	
+	public String getColor() {
+		return null;
+	}
+	
+}
