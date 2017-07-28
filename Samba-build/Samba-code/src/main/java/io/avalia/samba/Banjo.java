64380b16861f13e29b71226145ba0435a9a22b30
Lucas Galimberti Domingues <lucas.domingues@softplan.com.br>
2017-07-28 11:48:15 -0300

    closes #1

--- Samba-build/Samba-code/src/main/java/io/avalia/samba/Banjo.java
+++ Samba-build/Samba-code/src/main/java/io/avalia/samba/Banjo.java
@@ -3,7 +3,7 @@ package io.avalia.samba;
 public class Banjo implements IInstrument {
 	
 	public String play() {
-		return null;
+		return "dumdum";
 	}
 	
 	public int getSoundVolume() {
---
+++ Samba-build/Samba-tests/src/test/java/io/avalia/samba/BanjoTest.java
@@ -0,0 +1,28 @@
+package io.avalia.samba;
+
+import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+public class BanjoTest {
+
+  @Test
+  public void thereShouldBeAnIInstrumentInterfaceAndABanjoClass() {
+    IInstrument banjo = new Banjo();
+    assertNotNull(banjo);
+  }
+  
+  @Test
+  public void itShouldBePossibleToPlayAnInstrument() {
+    IInstrument banjo = new Banjo();
+    String sound = banjo.play();
+    assertNotNull(sound);
+  }
+  
+  @Test
+  public void aBanjoShouldMakePouet() {
+    IInstrument banjo = new Banjo();
+    String sound = banjo.play();
+    Assert.assertEquals("dumdum", sound);
+  }
+}
